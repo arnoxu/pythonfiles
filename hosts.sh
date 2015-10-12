8dd2372fb99ddb5138d51e5a1779651f2feeede9
sa <sa@ag866.com>
2015-10-12 13:21:19 +0800

    hosts.py -- for adding hosts into server.xml

---
+++ hosts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+path='/arno/shell'
+file=server.conf
+hosts1='/arno/shell/hosts.txt'
+hosts2='/arno/shell/wwwhosts.txt'
+
+for i in `cat $hosts1`
+do 
+    sed -i '1 a\        <Alias>$i</Alias>' $path/$file
+done
+
+
